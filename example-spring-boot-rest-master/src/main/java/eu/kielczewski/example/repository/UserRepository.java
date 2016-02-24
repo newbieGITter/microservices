b385e2459a8a4db8b224a2b14cbaaa8cbef5b8cc
saahas kulkarni <sahaas.kulkarni@ing.nl>
2016-02-24 05:28:12 +0100

    First commit

--- example-spring-boot-rest-master/src/main/java/eu/kielczewski/example/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package eu.kielczewski.example.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import eu.kielczewski.example.domain.User;
-
-public interface UserRepository extends JpaRepository<User, String> {
-}
